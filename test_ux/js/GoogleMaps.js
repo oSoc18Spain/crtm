0837ecd30426999bd452ea1bbb819d2c2d986d96
cpinamtz <carlospinamartinezdm@gmail.com>
2018-07-12 12:28:32 +0200

    Add results div with some info

--- test_ux/js/GoogleMaps.js
+++ test_ux/js/GoogleMaps.js
@@ -36,16 +36,36 @@ function searchmap_cb(results, status){
 			ll = {lat: results[0].geometry.location.lat(), 
 				lng: results[0].geometry.location.lng()};
 
-			// sl = SM.search(ll,1000);
-
 			radius = 100;
+
+			sl = SM.search(ll, radius)
 
 			while((sl = SM.search(ll, radius)).length < MAX_RESULTS)
 				radius += 100;
-				addstation(sl);
+			addstation(sl);
 
-			sl = sl[0, MAX_RESULTS - 1]
-			
+			document.getElementById('results-button').style.display = ""
+			console.log(sl)
+			sl.forEach(function(elm, idx){
+				var result = document.getElementById('result-row')
+				var clone = result.cloneNode(true)
+				clone.style.display = ""
+				clone.id = result.id + idx
+				var icon = clone.getElementsByClassName("col-md-1")[0]
+				if(elm['type'] == 0){
+					icon.classList.add("fas")
+					icon.classList.add("fa-subway")
+					icon.classList.add("fa-lg")
+				}
+				else{
+					icon.classList.add("fas")
+					icon.classList.add("fa-bus")
+					icon.classList.add("fa-lg")
+				}
+				var typeStation = clone.getElementsByClassName("col-md-4")[0]
+				typeStation.innerHTML = elm['name']
+				result.parentNode.appendChild(clone)
+			});
 
 		}else{
 			console.log('Geocode error: ' + status);
